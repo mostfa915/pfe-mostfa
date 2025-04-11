58dfcbf3007500441f17611d217a03dc9d03e1f0
mostfa915 <73286348+mostfa915@users.noreply.github.com>
2025-04-11 12:21:16 +0100

    Add files via upload

---
+++ pfe/pfe-backend/src/main/java/com/example/pfe_p/PfePApplication.java
@@ -0,0 +1,13 @@
+package com.example.pfe_p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfePApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfePApplication.class, args);
+	}
+
+}
